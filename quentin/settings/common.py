6ff6eb5a315b391fdfa2d10f3f45eb0d9876309b
Bruno L. Carli <brunolcarli@gmail.com>
2022-02-19 16:32:57 -0300

    ADD redis env vars

--- quentin/settings/common.py
+++ quentin/settings/common.py
@@ -16,3 +16,8 @@ TOKEN = os.environ.get('TOKEN')
 TMDB_API = 'https://api.themoviedb.org/3/'
 TMDB_KEY = {'api_key': os.getenv('TMDB_KEY')}
 IMG_URL = 'https://image.tmdb.org/t/p/w500/'
+
+
+# Redis (cache)
+REDIS_HOST = os.environ.get('REDIS_HOST')
+REDIS_PORT = os.environ.get('REDIS_PORT')
