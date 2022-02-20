67ba79e1e66c8c79945d01d99389391dcf5870b8
Bruno L. Carli <brunolcarli@gmail.com>
2022-02-20 17:19:00 -0300

    ADD upload and download commands

--- core/commands.py
+++ core/commands.py
@@ -1,6 +1,10 @@
 """
-Bot valid commands definition.
+Bot available commands definition.
 """
+from io import BytesIO
+import json
+import requests
+from base64 import b64encode, b64decode
 import discord
 from discord.ext import commands
 from discord import ActionRow, Button, ButtonStyle
@@ -9,6 +13,7 @@ from ast import literal_eval as evl
 from quentin.settings.common import __version__, IMG_URL, REDIS_HOST, REDIS_PORT
 from core.api_call import get_trending
 from core.util import paginate
+from core.db_handler import Database
 
 
 client = commands.Bot(command_prefix='/')
@@ -17,7 +22,6 @@ cache = Redis(
     port=REDIS_PORT,
     decode_responses=True
 )
-
 
 
 @client.event
@@ -245,3 +249,54 @@ async def get_trends(ctx, *args):
             )
         # Store sent messages for later edition if button clicked
         responses.append(response)
+
+
+@client.command(aliases=['send_file'])
+async def upload(ctx):
+    files = ctx.message.attachments
+    user_id = ctx.message.author.id
+    if not files:
+        return await ctx.send('You must upload a file.')
+
+    # validate files extension
+    valid_extensions = ('image/png', 'video/mp4')
+    for file in files:
+        if file.content_type not in valid_extensions:
+            return await ctx.send(f'Invalid file extension {file.content_type}')
+
+    db = Database()
+    for file in files:
+        extension = file.content_type.split('/')[1]
+        file_request = requests.get(file.url)
+        dumped = json.dumps(b64encode(file_request.content).decode('utf-8'))
+        item_id = db.insert(user_id, dumped, json.dumps(file.url), json.dumps(extension))
+        await ctx.send(f'Inserted file on database with ID: {item_id}')
+    db.close()
+
+
+@client.command(aliases=['get_file'])
+async def download(ctx, item_id=None):
+    if not item_id:
+        return await ctx.send('Must inform item ID.')
+
+    db = Database()
+    results = db.select(item_id)
+
+    if not results:
+        return await ctx.send(f'No file found for ID: {item_id}')
+
+    record = results[0]
+    id, user_id, dump, url, extension, date = record
+    file = b64decode(dump)
+
+    embed = discord.Embed(color=0x1E1E1E, type='rich')
+    embed.add_field(name='Stored by:', value=user_id, inline=False)
+    embed.add_field(name='File URL:', value=f'`{url}`', inline=True)
+    embed.add_field(name='Stored at:', value=str(date), inline=True)
+    db.close()
+
+    await ctx.send(
+        f'Item ID: {id}',
+        embed=embed,
+        file=discord.File(BytesIO(file), filename=f'file.{extension}')
+    )
